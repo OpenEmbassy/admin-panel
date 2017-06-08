a33aa003c9fc81edad51d6a76325d76305ce1063
Saad Hasan <saadronida@gmail.com>
2017-06-08 19:00:00 +0200

    login page with auth, but still need some styling and changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component'
 import { UsersComponent } from './users/users.component';
 import { QuestionsComponent } from './questions/questions.component';
+import { Login } from './login/login.component'
 
 const routes: Routes = [
-{
-  path: '',
-  children: [{
-    path: 'users',
-    component: UsersComponent
-  }, {
-    path: 'questions',
-    component: QuestionsComponent
-  }, {
-    path: '',
-    pathMatch: 'full',
-    component: UsersComponent,
-  }]
-}
+{ path:'login', component:Login },
+{ path: 'users', component: UsersComponent },
+{ path: 'questions', component: QuestionsComponent }
 ];
 
 @NgModule({
--- src/app/data/api.service.ts
+++ src/app/data/api.service.ts
@@ -2,8 +2,6 @@ import { Injectable } from '@angular/core';
 import { Http, RequestOptions, Headers } from '@angular/http';
 import { environment } from '../../environments/environment';
 
-const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3N1ZXIiOiJvcGVuZW1iYXNzeSIsInVzZXJJZCI6MywidXNlclR5cGUiOiJhZG1pbiIsImV4cCI6MTQ5Nzk5ODA4NywiaWF0IjoxNDk1NDA2MDg3fQ.g3UyhVcGQPRlvTnj3nCGJA73Re2Ad8ZjZEzW12jbvjQ'
-
 @Injectable()
 export class ApiService {
   
@@ -11,8 +9,22 @@ export class ApiService {
   private http: Http
   ) {}
   
+  login(credentials) {
+    return this.http.post('http://localhost:4050/v1/auth/login',credentials)
+      .map(response => response.json());
+  }
+  // if (res.status !== 402 || `${res.header.responseType}`) {
+    // location.href('localhost/login')
+    // next()
+    // alert(err.msg)
+  // }
+  logout() {
+    localStorage.removeItem('x-access-token'); 
+  }
+
   getQuestions() {
     return this.http.get('http://localhost:4050/v1/admin/questions', this.getRequestOptions())
+
     .map(response => response.json())
     // a loop for testing purposes only 
     .map(questions => {
@@ -32,7 +44,7 @@ export class ApiService {
   getRequestOptions(): RequestOptions {
     const headers = new Headers()
     headers.append('Content-Type', 'application/json')
-    headers.append('x-access-token', TOKEN)
+    headers.append('x-access-token', localStorage.getItem('x-access-token'))
     return new RequestOptions ({ headers})
   }
   
