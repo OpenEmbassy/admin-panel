832fa6b65a37e9dbd2aa62b8cd1fe347edd49982
HackYourFuture <hackyourfuture@HackYourFutures-MacBook-Pro.local>
2017-06-11 13:21:58 +0200

    .

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -12,15 +12,34 @@ import { NgForm } from '@angular/forms';
 
 export class LoginComponent {
   
-  constructor(private _auth: ApiService) {}
-
+  constructor(private _auth: ApiService, private _Router: Router) {}
+  
   onLogin(email, password) {
     // Validate inputs
     const credentials = {email, password}
     this._auth.login(credentials)
-              .subscribe(data => localStorage.setItem('x-access-token', data.jwt),
-                          err => alert(err));
-    location.href="users"
-    //this.router.navigate(['login']);
+    .subscribe(data => {
+      localStorage.setItem('x-access-token', data.jwt)
+      localStorage.setItem('userType', data.profile.type)
+      localStorage.setItem('userName', data.profile.firstName)
+      alert('Welcome '+ data.profile.firstName)
+      this._Router.navigate(['questions'])
+    },
+    
+    err => { 
+      if(err.status == "403" || err.status == "404"){ // 403= wrong password, 404= wrong email
+        alert('wrong email or password')
+        this._Router.navigate(['login']);
+      } else if(err.status == "400"){
+        alert('Please fill all fields') 
+        this._Router.navigate(['login']);
+      }
+      else {
+        alert(err)   // other errors
+        this._Router.navigate(['login']);
+      }
+    }
+    );   
+    this._Router.navigate(['']);
   }
 }
