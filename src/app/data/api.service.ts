de2027a8fa31b8e79d1a1117c5b4fb97391ed169
HackYourFuture <hackyourfuture@HackYourFutures-MacBook-Pro.local>
2017-06-11 13:27:17 +0200

    .

--- src/app/data/api.service.ts
+++ src/app/data/api.service.ts
@@ -31,64 +31,72 @@ export interface UserPageData {
 
 @Injectable()
 export class ApiService {
-
+  
   constructor(
-    private http: Http
+  private http: Http
   ) { }
-
+  
   login(credentials) {
     return this.http.post('http://localhost:4050/v1/auth/login', credentials)
-      .map(response => response.json());
+    .map(response => response.json());
   }
   // if (res.status !== 402 || `${res.header.responseType}`) {
-  // location.href('localhost/login')
-  // next()
-  // alert(err.msg)
-  // }
-  logout() {
-    localStorage.removeItem('x-access-token');
-  }
-
-  getQuestions(page: number, pageSize: number, status?: string) {
-    const options = this.getRequestOptions()
-    const params = new URLSearchParams()
-    params.set('page', page.toString())
-    params.set('pageSize', pageSize.toString())
-    if (status) {
-      params.set('status', status)
+    // location.href('localhost/login')
+    // next()
+    // alert(err.msg)
+    // }
+    
+    isLoggedIn(){
+      if(localStorage.getItem('userType') === 'admin'){
+        return true;
+      }
     }
-    options.search = params
-    return this.http.get('http://localhost:4050/v1/admin/questions', options)
+    
+    logout() {
+      localStorage.removeItem('x-access-token');
+    }
+    
+    getQuestions(page: number, pageSize: number, status?: string) {
+      const options = this.getRequestOptions()
+      const params = new URLSearchParams()
+      params.set('page', page.toString())
+      params.set('pageSize', pageSize.toString())
+      if (status) {
+        params.set('status', status)
+      }
+      options.search = params
+      return this.http.get('http://localhost:4050/v1/admin/questions', options)
       .map(response =>  {
         const data = response.json();
         data.page = page;
         return data;
       })
-  }
-
-getUsers(page: number, pageSize: number, type?: string) {
-    const options = this.getRequestOptions()
-    const params = new URLSearchParams()
-    params.set('page', page.toString())
-    params.set('pageSize', pageSize.toString())
-    if (type) {
-      params.set('type', type)
     }
-    options.search = params
-    return this.http.get('http://localhost:4050/v1/admin/users', options)
+    
+    getUsers(page: number, pageSize: number, type?: string) {
+      const options = this.getRequestOptions()
+      const params = new URLSearchParams()
+      params.set('page', page.toString())
+      params.set('pageSize', pageSize.toString())
+      if (type) {
+        params.set('type', type)
+      }
+      options.search = params
+      return this.http.get('http://localhost:4050/v1/admin/users', options)
       .map(response =>  {
         const data = response.json();
         data.page = page;
         return data;
       })
+    }
+    
+    
+    getRequestOptions(): RequestOptions {
+      const headers = new Headers()
+      headers.append('Content-Type', 'application/json')
+      headers.append('x-access-token', localStorage.getItem('x-access-token'))
+      return new RequestOptions({ headers })
+    }
+    
   }
-
-
-  getRequestOptions(): RequestOptions {
-    const headers = new Headers()
-    headers.append('Content-Type', 'application/json')
-    headers.append('x-access-token', localStorage.getItem('x-access-token'))
-    return new RequestOptions({ headers })
-  }
-
-}
+  
